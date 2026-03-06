ffbdfe2bc5ac2494959a268e9c40d807d5d1f57f
mothy-08 <119960367+mothy-08@users.noreply.github.com>
2026-03-06 13:57:35 +0800

    update: data is fetched instead of hardcoding it

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,26 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import type { Category, Timeframes } from "./types";
 import CardProfile from "./components/CardProfile.vue";
 import CardCategory from "./components/CardCategory.vue";
 
-const data: Category[] = [
-  {
-    title: "Work",
-    timeframes: {
-      daily: {
-        current: 5,
-        previous: 7,
-      },
-      weekly: {
-        current: 32,
-        previous: 36,
-      },
-      monthly: {
-        current: 103,
-        previous: 128,
-      },
-    },
-  },
-  {
-    title: "Play",
-    timeframes: {
-      daily: {
-        current: 1,
-        previous: 2,
-      },
-      weekly: {
-        current: 10,
-        previous: 8,
-      },
-      monthly: {
-        current: 23,
-        previous: 29,
-      },
-    },
-  },
-  {
-    title: "Study",
-    timeframes: {
-      daily: {
-        current: 0,
-        previous: 1,
-      },
-      weekly: {
-        current: 4,
-        previous: 7,
-      },
-      monthly: {
-        current: 13,
-        previous: 19,
-      },
-    },
-  },
-  {
-    title: "Exercise",
-    timeframes: {
-      daily: {
-        current: 1,
-        previous: 1,
-      },
-      weekly: {
-        current: 4,
-        previous: 5,
-      },
-      monthly: {
-        current: 11,
-        previous: 18,
-      },
-    },
-  },
-  {
-    title: "Social",
-    timeframes: {
-      daily: {
-        current: 1,
-        previous: 3,
-      },
-      weekly: {
-        current: 5,
-        previous: 10,
-      },
-      monthly: {
-        current: 21,
-        previous: 23,
-      },
-    },
-  },
-  {
-    title: "Self Care",
-    timeframes: {
-      daily: {
-        current: 0,
-        previous: 1,
-      },
-      weekly: {
-        current: 2,
-        previous: 2,
-      },
-      monthly: {
-        current: 7,
-        previous: 11,
-      },
-    },
-  },
-];
+const data = ref<Category[] | null>(null);
+
+async function loadData() {
+  try {
+    const response = await fetch(`${import.meta.env.BASE_URL}data.json`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status}`);
+    }
+
+    data.value = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+onMounted(loadData);
 
 const currentTimeframe = ref<Timeframes>("weekly");
 
