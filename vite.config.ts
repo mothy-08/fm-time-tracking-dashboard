98e4e992114865911aa027ad745854233a5aeb83
mothy-08 <119960367+mothy-08@users.noreply.github.com>
2026-03-06 13:15:22 +0800

    add: github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/fm-time-tracking-dashboard/",
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
